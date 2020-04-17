cb22545e83112ecd5595763f1798617dfaae5d06
Ciara Jhenica Estavillo <estavillocj43@gmail.com>
2020-04-17 18:37:35 +0800

    did some modifications based on tutorials in React website and DigitalOcean

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface MyMessageProps {
+  message: string;
+}
+
+function MyMessage({ message }: MyMessageProps) {
+  return <div>I shall speak! My message is: {message}</div>;
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <MyMessage message={"Hello"} />
+        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
